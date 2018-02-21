50080b5da2736b96e59c290e76ef976c41a53553
Raman Prasad <raman_prasad@harvard.edu>
2018-02-21 14:55:42 -0500

    updated preprocess

--- preprocess/code/preprocess.py
+++ preprocess/code/preprocess.py
@@ -9,7 +9,7 @@ INPUT_DIR = join(dirname(CURRENT_DIR), 'input')
 OUTPUT_DIR = join(dirname(CURRENT_DIR), 'output')
 
 
-def test_run(input_file):
+def test_run(input_file, output_filepath):
     """Main test run class for this module"""
 
     runner, err_msg = PreprocessRunner.load_from_csv_file(input_file)
@@ -26,7 +26,13 @@ def test_run(input_file):
     jstring = runner.get_final_json(indent=4)
     print(jstring)
 
+    open(output_filepath, 'w').write(jstring)
+
+    print('file written: %s' % output_filepath)
+
 
 if __name__ == '__main__':
-    INPUT_FILE = join(INPUT_DIR, 'test_file_01.csv')
-    test_run(INPUT_FILE)
+    input_csv = join(INPUT_DIR, 'test_file_01.csv')
+    output_file = join(OUTPUT_DIR, 'test_file_01_preprocess.json')
+
+    test_run(input_csv, output_file)
