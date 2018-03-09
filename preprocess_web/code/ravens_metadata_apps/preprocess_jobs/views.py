0cec33dfca6a2065633dcac52f5cdcbf69f4184a
Raman Prasad <raman_prasad@harvard.edu>
2018-03-09 15:02:04 -0500

    updated code.... (#40)

--- preprocess_web/code/ravens_metadata_apps/preprocess_jobs/views.py
+++ preprocess_web/code/ravens_metadata_apps/preprocess_jobs/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
@@ -28,14 +29,80 @@ def view_basic_upload_form(request):
 
             redirect_url = reverse('show_job_info',
                                    kwargs=dict(job_id=job.id))
+
             return HttpResponseRedirect(redirect_url)
-            return JsonResponse(job.as_dict())
     else:
         form = PreprocessJobForm()
 
     return render(request,
                   'preprocess/view_basic_upload_form.html',
                   {'form': form})
+
+
+
+
+@csrf_exempt
+def endpoint_api_single_file(request):
+    """Preprocess a single file
+    - Always returns JSON
+    - If not a POST:
+        - return error
+        - success:
+
+    - General message format:
+            { "success" : true/false,
+              "message" : "e.g if error, put message here",
+              "status" : finished/not finished
+              "state" : what stage, e.g. preprocess started...
+              "progress" : % completed
+              "preprocess_status_url" : gives details of PreprocessInfo
+             }
+    """
+    if request.method != 'POST':
+        user_msg = dict(success=False,
+                        message='Please use a POST request')
+        return JsonResponse(user_msg,
+                            status=412)
+
+    form = PreprocessJobForm(request.POST, request.FILES)
+    if not form.is_valid():
+
+        user_msg = dict(success=False,
+                        message='Errors found',
+                        errors=form.errors.as_json())
+        return JsonResponse(user_msg,
+                            status=400)
+
+
+    # save the PreprocessJob
+    job = form.save()
+
+    # start background task
+    JobUtil.start_preprocess(job)
+
+    user_msg = dict(success=True,
+                    message='some message',
+                    callback_url=job.get_job_status_link(),
+                    data=job.as_dict())
+
+    return JsonResponse(user_msg)
+
+"""
+import requests
+import os
+from os.path import isfile, isdir, join
+url = 'http://127.0.0.1:8000/preprocess/api-single-file'
+
+test_file_dir = '/Users/ramanprasad/Documents/github-rp/raven-metadata-service/preprocess/input/'
+
+for fname in os.listdir(test_file_dir):
+    if fname.endswith('.csv'):
+        fullname = join(test_file_dir, fname)
+        files = {'source_file': open(file_path, 'rb')}
+        r = requests.post(url, files=files)
+        r.text
+
+"""
 
 def show_job_info(request, job_id):
     """test to show uploaded file info"""
@@ -45,5 +112,9 @@ def show_job_info(request, job_id):
         raise Http404('job_id not found: %s' % job_id)
 
     JobUtil.check_status(job)
+
+    if 'pretty' in request.GET:
+        jstring = json.dumps(job.as_dict(), indent=4)
+        return HttpResponse('<pre>%s</pre>' % jstring)
 
     return JsonResponse(job.as_dict())
