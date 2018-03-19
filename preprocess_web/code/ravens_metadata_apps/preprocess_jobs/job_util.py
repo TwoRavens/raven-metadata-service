57b5ce4795cd0974a6178030f62731f9f6897ebf
Raman Prasad <raman_prasad@harvard.edu>
2018-03-19 11:50:35 -0400

    update to use id

--- preprocess_web/code/ravens_metadata_apps/preprocess_jobs/job_util.py
+++ preprocess_web/code/ravens_metadata_apps/preprocess_jobs/job_util.py
@@ -20,9 +20,9 @@ class JobUtil(object):
         """Start the preprocessing!"""
         assert isinstance(job, PreprocessJob),\
                'job must be a PreprocessJob'
-        job_id = uuid.UUID.time
+
         # send the file to the queue
-        task = preprocess_csv_file.delay(job.source_file.path, job_id=job_id)
+        task = preprocess_csv_file.delay(job.source_file.path, job_id=job.id)
 
         # set the task_id
         job.task_id = task.id
