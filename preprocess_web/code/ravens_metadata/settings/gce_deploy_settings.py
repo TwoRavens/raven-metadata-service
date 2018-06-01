cbd5fa48fc6bdf42bf0399f32184dfffd1952668
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2018-06-01 14:46:48 -0400

    kubernetes gce deployment

---
+++ preprocess_web/code/ravens_metadata/settings/gce_deploy_settings.py
@@ -0,0 +1,31 @@
+import os, json
+from os.path import isfile, join
+from distutils.util import strtobool
+
+from .docker_test_settings import *
+from google.oauth2 import service_account
+
+# -----------------------------------
+# use Google Cloud Storage via django-storages
+# -----------------------------------
+
+# add to apps
+#
+INSTALLED_APPS.append('storages')
+
+# set as default
+#
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+
+# bucket name / project id
+#
+GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME',
+                                '2ravens-metadata-dev-storage')
+
+GS_PROJECT_ID = os.environ.get('GS_PROJECT_ID',
+                               'raven2-186120')
+
+# Credentials from file info
+#
+GCE_CREDS_INFO = json.loads(os.environ.get('GCE_CREDS_INFO', 'nothing found'))
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(GCE_CREDS_INFO)
