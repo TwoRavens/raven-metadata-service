8899b7b825f995e27e8584fee09bad6afa446586
aaron <you@example.com>
2019-07-31 09:00:03 -0500

    R preprocess results

--- preprocess/code/test_dataverse.py
+++ preprocess/code/test_dataverse.py
@@ -1,6 +1,17 @@
 import glob
+import json
 import subprocess
+import sys
 
 for file in glob.glob('../../test_data/dataverse/data/*'):
     filename = file.split('/')[-1]
-    subprocess.run(f'python preprocess.py "{file}" "../../test_data/dataverse/python/{filename}"', shell=True)
+    cmd = f'python preprocess.py "{file}" "../../test_data/dataverse/python/{filename}"'
+    if sys.argv[1]:
+        cmd = f'Rscript ../../rscripts/runPreprocess.R "{file}" ../../rscripts/'
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    try:
+        obj = json.loads(result.stdout.decode('utf8').split('---START-PREPROCESS-JSON---')[1].split('---END-PREPROCESS-JSON---')[0])
+    except:
+        continue 
+    with open(f'../../test_data/dataverse/R/{filename}', 'w') as f:
+        json.dump(obj, f) 
