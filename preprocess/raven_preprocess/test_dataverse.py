74c154117e90c4a8664c54e046a2416614e88d22
Raman Prasad <raman_prasad@harvard.edu>
2019-08-30 13:03:10 -0400

    updated paths so that unittest may be run from the /preprocess directory

--- preprocess/raven_preprocess/test_dataverse.py
+++ preprocess/raven_preprocess/test_dataverse.py
@@ -5,19 +5,26 @@ import sys
 
 import dictdiffer
 import matplotlib.pyplot as plt
-import pandas as pd 
+import pandas as pd
 
 from raven_preprocess.preprocess_runner import PreprocessRunner
 
+from os.path import \
+    (abspath, basename, dirname, isdir, isfile, join, splitext)
+
+
+CURRENT_DIR = dirname(abspath(__file__))
+TEST_DATA_DIR = join(dirname(dirname(CURRENT_DIR)), 'test_data')
+
 def get_path(filename, where='python'):
-    return f'../../test_data/dataverse/{where}/{filename}'
+    return f'{TEST_DATA_DIR}/dataverse/{where}/{filename}'
 
 replace = dict(
     binary = 'defaultBinary',
     cdfPlotType = 'cdfplottype',
     cdfPlotX = 'cdfplotx',
     cdfPlotY = 'cdfploty',
-    description = 'labl', 
+    description = 'labl',
     fewestFreq = 'freqfewest',
     fewestValues = 'fewest',
     herfindahlIndex = 'herfindahl',
@@ -38,14 +45,14 @@ replace = dict(
     pdfPlotY = 'ploty',
     stdDev = 'sd',
     time = 'defaultTime',
-    validCount = 'valid', 
+    validCount = 'valid',
     variableName = 'varnamesSumStat',
     uniqueCount = 'uniques'
 )
 
 ignore = 'cdfPlotType pdfPlotType plotValues'.split()
 ignore += 'cdfPlotX cdfPlotY pdfPlotX pdfPlotY'.split()
-ignore += ['interval', 'min', 'max', 'nature', 'numchar', 'variableName']  # differ in R being wrong 
+ignore += ['interval', 'min', 'max', 'nature', 'numchar', 'variableName']  # differ in R being wrong
 ignore += ['fewestFreq', 'fewestValues', 'midpoint', 'midpointFreq', 'mode', 'modeFreq'] # differ in how calulated and num of results
 ignore += ['binary', 'invalidCount', 'validCount', 'uniqueCount', 'herfindahlIndex'] # differ in missingness
 ignore += ['mean', 'median', 'stdDev'] # differ in rounding
@@ -65,11 +72,11 @@ def diff(filename, py_path, R_path):
     try:
         R_obj = json.load(open(R_path))
     except:
-        R_obj = {} 
+        R_obj = {}
 
     R_obj1 = dict(variables={})
     for var in R_obj.get('variables', []):
-        R_obj1['variables'][var] = {} 
+        R_obj1['variables'][var] = {}
         for (k, k1) in replace.items():
             val = R_obj['variables'][var].get(k1)
             if val == 0.0 and k == 'modeFreq' or val =='NULL':
@@ -111,10 +118,10 @@ def diff(filename, py_path, R_path):
         if df3['x'].any() and df3['y'].any():
             df3.plot(x='x', y='y', ax=ax3)
 
-        plt.savefig(f'../../test_data/dataverse/plots/{filename.split(".")[0]}_{var}.png')
+        plt.savefig(f'{TEST_DATA_DIR}/dataverse/plots/{filename.split(".")[0]}_{var}.png')
         plt.close(fig)
 
-    changes = [] 
+    changes = []
     for change in list(dictdiffer.diff(R_obj1, py_obj, ignore='self dataset variableDisplay'.split(), tolerance=0.01)):
         if change[0] == 'change' and change[2] not in [('yes', True), ('no', False), ('no', 'unknown')]:
             if not (change[2][0] == 'NA' and not isinstance(change[2][1], list) and pd.isna(change[2][1])):
@@ -125,7 +132,7 @@ def diff(filename, py_path, R_path):
             json.dump(changes, f, indent=2)
 
 errs = {}
-for file in glob.glob('../../test_data/dataverse/data/*'):
+for file in glob.glob('f{TEST_DATA_DIR}/dataverse/data/*'):
     filename = file.split('/')[-1]
     py_path = get_path(filename)
     R_path = get_path(filename, 'R')
@@ -153,8 +160,7 @@ for file in glob.glob('../../test_data/dataverse/data/*'):
     obj = json.loads(result.stdout.decode('utf8').split('---START-PREPROCESS-JSON---')[1].split('---END-PREPROCESS-JSON---')[0])
 
     with open(R_path, 'w') as f:
-        json.dump(obj, f) 
+        json.dump(obj, f)
 
 with open(get_path('errors.json'), 'w') as f:
-    json.dump(errs, f, indent=2) 
-
+    json.dump(errs, f, indent=2)
