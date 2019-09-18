4edbd07cfaa722d7e7eb8c64c3b811f5362675fa
aaron <you@example.com>
2019-09-18 06:19:20 -0500

    merge date geussing updates

--- preprocess/raven_preprocess/type_guess_util.py
+++ preprocess/raven_preprocess/type_guess_util.py
@@ -1,10 +1,13 @@
 """ Module for type guessing """
+import datetime
+
+import dateutil.parser
 import pandas as pd
 from pandas.api.types import is_float_dtype, is_numeric_dtype
 
 import raven_preprocess.col_info_constants as col_const
+from raven_preprocess.column_info import ColumnInfo
 from raven_preprocess.basic_utils.basic_err_check import BasicErrCheck
-
 
 class TypeGuessUtil(BasicErrCheck):
     """Check variable types of a dataframe"""
@@ -14,72 +17,52 @@ class TypeGuessUtil(BasicErrCheck):
 
         self.col_series = col_series
         self.col_info = col_info
+        self.col_info.time_val = col_const.UNKNOWN
         self.binary = False
-        # # final outout returned
+
+        # final outout returned
         self.check_types()
 
     def check_types(self):
         """check the types of the dataframe"""
         # assert self.colnames, 'self.colnames must have values'
-        # number of missing entries
-        #
+
         self.col_info.invalid = int(self.col_series.isnull().sum())
-
-        # number of valid entries
-        #
         self.col_info.valid = int(self.col_series.count())
-
-        # set time , what exactly we want to do with this
-        #
-        self.col_info.time_val = self.check_time(self.col_series)
 
         # Drop nulls...
         self.col_series.dropna(inplace=True)
 
-        uniques = self.col_series.unique()
+        self.col_info.binary = col_const.BINARY_YES if len(self.col_series.unique()) == 2 else col_const.BINARY_NO
 
-        binary = self.check_binary(len(uniques))
-
-        # set up binary values..
-        if binary:
-            self.col_info.binary = col_const.BINARY_YES
-        else:
-            self.col_info.binary = col_const.BINARY_NO
         if self.is_not_numeric(self.col_series) or self.is_logical(self.col_series):
-
+            self.col_info.time_val = self.check_time(self.col_series)
             self.col_info.numchar_val = col_const.NUMCHAR_CHARACTER
             self.col_info.default_interval = col_const.INTERVAL_DISCRETE
             self.col_info.nature = col_const.NATURE_NOMINAL
-
         else:
-
             try:
                 series_info = self.col_series.astype('int')
-            except ValueError as err_obj:
-                user_msg = ('Type guess error when converting'
-                            ' to int: %s') % err_obj
-                self.add_err_msg(user_msg)
+            except ValueError as e:
+                self.add_err_msg('Type guess error when converting to int: %s' % e)
                 return
 
             if any(series_info.isnull()):
                 # CANNOT REACH HERE B/C NULLS ARE DROPPED!
-                #
+
                 self.col_info.numchar_val = col_const.NUMCHAR_CHARACTER
                 self.col_info.nature = col_const.NATURE_NOMINAL
                 self.col_info.default_interval = col_const.INTERVAL_DISCRETE
             else:
                 self.col_info.numchar_val = col_const.NUMCHAR_NUMERIC
 
-                if is_float_dtype(series_info):
+                ints = self.col_series.where(lambda x: x is 0 or x % 1 == 0.0)
+                if is_float_dtype(self.col_series) and ints.count() != len(self.col_series):
                     self.col_info.default_interval = col_const.INTERVAL_CONTINUOUS
-                    self.col_info.nature = self.check_nature(series_info, True)
-
+                    self.col_info.nature = self.check_nature(self.col_series, True)
                 else:
                     self.col_info.default_interval = col_const.INTERVAL_DISCRETE
                     self.col_info.nature = self.check_nature(series_info, False)
-
-
-
 
     @staticmethod
     def is_not_numeric(var_series):
@@ -87,17 +70,10 @@ class TypeGuessUtil(BasicErrCheck):
         assert isinstance(var_series, pd.Series), \
             "var_series must be a pandas.Series. Found type: (%s)" % type(var_series)
 
-        var_series.dropna(inplace=True)
-        if var_series.size == 0:
-            # print("character")
-            return True
-        elif var_series.dtype == 'bool':
+        if var_series.size == 0 or var_series.dtype == 'bool':
             return True
 
-        if is_numeric_dtype(var_series):
-            return False
-        else:
-            return True
+        return not is_numeric_dtype(var_series)
 
     @staticmethod
     def is_logical(var_series):
@@ -105,21 +81,16 @@ class TypeGuessUtil(BasicErrCheck):
         assert isinstance(var_series, pd.Series), \
             "var_series must be a pandas.Series. Found type: (%s)" % type(var_series)
 
-        var_series.dropna(inplace=True)
-
         # Check the dtype
         #    "bool" - True, clearly logical
         #    "object" - possibly logical that had contained np.Nan
         #    ~anything else~ - False
-        #
         if var_series.dtype == 'bool':
             return True
         elif var_series.dtype != 'object':
             return False
 
-        # It's an object.  Check if all the values
-        #   either True or False
-        #
+        # It's an object.  Check if all the values either True or False
         total = var_series.size
         total_cnt = 0
         for val, cnt in var_series.value_counts().iteritems():
@@ -128,7 +99,6 @@ class TypeGuessUtil(BasicErrCheck):
 
         if total_cnt == total:
             # This is a boolean -- everything was either True or False
-            #
             return True
 
         return False
@@ -141,24 +111,20 @@ class TypeGuessUtil(BasicErrCheck):
                 return col_const.NATURE_PERCENT
             elif data_series.between(0, 100).all() and min(data_series) < 15 and max(data_series) > 85:
                 return col_const.NATURE_PERCENT
-            else:
-                return col_const.NATURE_RATIO
-
-        else:
-            return col_const.NATURE_ORDINAL
+            return col_const.NATURE_RATIO
+        return col_const.NATURE_ORDINAL
 
     @staticmethod
     def check_time(var_series):
-        """Unimplemented"""
+        """Check if Series is a datetime"""
         assert isinstance(var_series, pd.Series), \
             "var_series must be a pandas.Series. Found type: (%s)" % type(var_series)
 
-        return col_const.UNKNOWN
+        if var_series.dtype == 'object':
+            try:
+                var_series[:10].apply(lambda x: x.strip() and dateutil.parser.parse(x))
+                return True
+            except:
+                pass
 
-    @staticmethod
-    def check_binary(unique_size):
-        """ check if the series is binary or not """
-        if unique_size is 2:
-            return True
-        else:
-            return False
+        return col_const.UNKNOWN
